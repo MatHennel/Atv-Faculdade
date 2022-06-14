4368e9541efe0c66ad24a537c4f8ee4e981b9d17
MatHennel <matheushennel123@gmail.com>
2022-06-14 20:02:44 -0300

    introdução dos getters e setters, métodos construtores e toString

--- Java/Orientação a Objetos/atividade-03-MatHennel/ContaCorrente.java
+++ Java/Orientação a Objetos/atividade-03-MatHennel/ContaCorrente.java
@@ -1,27 +1,100 @@
 public class ContaCorrente {
-    
-    int numeroDaConta;
-    int agencia;
-    String documento;
-    String senha;
-    boolean ativa;
-    double saldo;
 
-    void depositar(double valor){
+    private int numeroDaConta;
+    private int agencia;
+    private String documento;
+    private String senha;
+    private boolean ativa;
+    private double saldo;
+
+    public ContaCorrente(){
+
+    }
+
+    public ContaCorrente(int numeroDaConta,int agencia){
+        this.numeroDaConta = numeroDaConta;
+        this.agencia = agencia;
+    }
+
+    public ContaCorrente(int numeroDaConta,int agencia,boolean ativa){
+        this.numeroDaConta = numeroDaConta;
+        this.agencia = agencia;
+        this.ativa = ativa;
+    }
+
+
+    public void primeiroDeposito(double saldo){
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
         saldo = saldo + valor;
     }
 
-    
-    void sacar(double valor){
-        if(saldo >= valor){
+    public void sacar(double valor) {
+        if (saldo >= valor) {
             saldo = saldo - valor;
-        }
-        else{
+        } else {
             System.out.println("Saldo insuficiente!");
         }
-    
+
     }
-    
-    
+
+    public int getnumeroDaConta() {
+        return numeroDaConta;
+    }
+
+    public void setnumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public boolean getAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public String toString1(){
+        String str = "\t - Numero da Conta: "+ numeroDaConta;
+        str = str + "\n\t - Numero da Agencia: "+ agencia;
+        str = str + "\n\t - Documento: "+ documento;
+        str = str + "\n\t - Senha:  "+ senha;
+        str = str + "\n\t - Status:  "+ ativa;
+
+
+        return str;
+    }
+
+    public String toString2(){
+        
+        
+        return "Seu saldo é " + saldo;
+    }
 
 }
--- Java/Orientação a Objetos/atividade-03-MatHennel/Main.java
+++ Java/Orientação a Objetos/atividade-03-MatHennel/Main.java
@@ -1,20 +1,15 @@
 import java.util.Scanner;
 
-import javax.annotation.processing.SupportedOptions;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ContaCorrente conta = new ContaCorrente();
+        ContaCorrente conta = new ContaCorrente(29292929,543,true);
         Pessoa pessoa = new Pessoa();
 
-        conta.numeroDaConta = 4212444;
-        conta.agencia = 225;
-        conta.saldo = 1000;
-        conta.documento = "122-222";
-        conta.senha = "****";
-        conta.ativa = false;
+        
 
         int op = 0;
         int sair = 0;
@@ -29,65 +24,52 @@ public class Main {
 
             switch (op) {
                 case 1:
-                    // pular();
+
                     cadastro(pessoa, conta, ler);
-                    /*
-                     * System.out.println(" \t------------ Cadastro ------------ ");
-                     * System.out.print(" \tDigite seu nome: ");
-                     * pessoa.nome = ler.next();
-                     * System.out.print(" \tDigite seu cpf: ");
-                     * pessoa.cpf = ler.next();
-                     * conta.documento = pessoa.cpf;
-                     * System.out.print(" \tDigite sua idade: ");
-                     * pessoa.idade = ler.nextInt();
-                     * System.out.print(" \tDigite seu salario: ");
-                     * pessoa.salario = ler.nextInt();
-                     * System.out.print(" \tDigite a senha: ");
-                     * conta.senha = ler.next();
-                     */
+                    
                     break;
 
                 case 2:
+                    
+                    printConta(conta);
+
+                    break;
+
+                case 3:
                     System.out.print("Saque: ");
                     valor = ler.nextInt();
                     conta.sacar(valor);
 
                     break;
 
-                case 3:
+                case 4:
                     System.out.print("Deposito: ");
                     valor = ler.nextInt();
                     conta.depositar(valor);
 
                     break;
 
-                case 4:
-                    System.out.println("Seu saldo é: " + conta.saldo);
+                case 5:
+                    System.out.println(conta.toString2());
 
                     break;
-                case 5:
+                case 6:
 
                     sair = 1;
                     break;
             }
         }
-
-        // conta.depositar(10000);
-        // System.out.println("Saldo apos deposito: " + conta.saldo);
-
-        // conta.sacar(1000000);
-        // System.out.println("Saldo apos saque: " + conta.saldo);
-
     }
 
     public static void printar(ContaCorrente conta) {
         System.out.print("\n");
         System.out.println(" \t------------- Banco ------------- ");
         System.out.println(" \t1) Criar nova conta corrente ---- ");
-        System.out.println(" \t2) Sacar ------------------------ ");
-        System.out.println(" \t3) Depositar -------------------- ");
-        System.out.println(" \t4) Saldo da conta --------------- ");
-        System.out.println(" \t5) Sair --------------------------");
+        System.out.println(" \t2) Dados da conta ---------------");
+        System.out.println(" \t3) Sacar ------------------------ ");
+        System.out.println(" \t4) Depositar -------------------- ");
+        System.out.println(" \t5) Saldo da conta --------------- ");
+        System.out.println(" \t6) Sair -------------------------");
         System.out.println(" \t--------------------------------- ");
         System.out.print(" \t>");
 
@@ -102,15 +84,27 @@ public class Main {
     public static void cadastro(Pessoa pessoa, ContaCorrente conta, Scanner ler) {
         System.out.println(" \t------------ Cadastro ------------ ");
         System.out.print(" \tDigite seu nome: ");
-        pessoa.nome = ler.next();
+        System.out.print("");
+        ler.nextLine();
+        pessoa.setNome(ler.nextLine());
         System.out.print(" \tDigite seu cpf: ");
-        pessoa.cpf = ler.next();
-        conta.documento = pessoa.cpf;
+        pessoa.setCpf(ler.next());
+        conta.setDocumento(pessoa.getCpf());
         System.out.print(" \tDigite sua idade: ");
-        pessoa.idade = ler.nextInt();
+        pessoa.setIdade(ler.nextInt());
         System.out.print(" \tDigite seu salario: ");
-        pessoa.salario = ler.nextInt();
+        pessoa.setSalario(ler.nextInt());
+        conta.primeiroDeposito(pessoa.getSalario());
         System.out.print(" \tDigite a senha: ");
-        conta.senha = ler.next();
+        conta.setSenha(ler.next());
     }
+
+    public static void printConta(ContaCorrente conta){
+        System.out.println(" \t------------ Dados da Conta ------------ ");
+        System.out.println(conta.toString1());
+    }
+
+    
+
 }
+
