401e29850ca075e0d5e4bccf072f6514cdfb5547
MatHennel <matheushennel123@gmail.com>
2022-06-17 20:03:18 -0300

    Conclusão do método buscaBinário e adição do método buscaSequencial

--- Java/Estrutura de Dados/Busca sequencial e binária/BuscaBinaria.java
+++ Java/Estrutura de Dados/Busca sequencial e binária/BuscaBinaria.java
@@ -13,12 +13,27 @@ public class BuscaBinaria {
         
 
         
-
+        
         tabela = buscaBinaria(v,tabela,ler,i);
+        System.out.println();
+        System.out.println("Tabela de tempo busca binaria:");
 
         int tamVetor = 10;
         for(int j = 0;j < 5;j++){
-            System.out.println("Tempo vetor ["+tamVetor+"]: " + tabela[j]);
+            System.out.println("| Tempo vetor ["+tamVetor+"]: " + tabela[j]);
+            tamVetor = tamVetor * 10;
+        }
+
+        i = 0;
+
+        tabela = buscaExaustiva(v, tabela, ler, i);
+        System.out.println();
+        System.out.println("Tabela de tempo busca exaustiva");
+
+        tamVetor = 10;
+
+        for(int j = 0;j < 5;j++){
+            System.out.println("| Tempo vetor ["+tamVetor+"]: " + tabela[j]);
             tamVetor = tamVetor * 10;
         }
        
@@ -30,26 +45,26 @@ public class BuscaBinaria {
             int inicio = 0;
             int fim = v.length - 1;
             int meio = 0;
-            int chave = 0;
+            int chave;
             long tempoInicial = 0;
             long tempoFinal = 0;
             Random aleatorio = new Random();
+            boolean achou = false;
             
 
-           
+            if(v.length == 1000000){
+                return tabela;
+            }
 
             
 
             for(int j = 0;j < v.length;j++){
-                v[j] = aleatorio.nextInt(100);
+                v[j] = aleatorio.nextInt(1000);
             }
 
             Arrays.sort(v);
-            int k = 0;
-            for(int j = 0;j < v.length;j++){
-                k = j + 1;
-                System.out.print(v[j] + "["+k+"]" + " ");
-            }
+
+
             
             System.out.print("Digite o numero que esta procurando: ");
             chave = ler.nextInt();            
@@ -62,45 +77,78 @@ public class BuscaBinaria {
 
             meio = (inicio + fim)/2;
 
-            if(chave > v[meio]){
+            if(chave == v[meio]){
+                i++;
+                v = new int[v.length*10];
+                achou = true;
+                break;
+            }
+
+            else if(chave > v[meio]){
                 inicio = meio + 1;
             }
-            else if(chave < v[meio]){
-                fim = meio - 1;
-            }
-            else if(chave == v[meio]){
-                i++;
-                v = new int[v.length*10];
-                System.out.println("ACHEIIIIIIIIIIIIIIIII");
-                
-                
-            }
             else{
-                i++;
-                v = new int[v.length*10];
-                System.out.println("NAOOOOO ACHEIIIIIIIIIIIIIIIII");
-            }
+                fim = meio - 1;
+            }      
+        }
 
-           
-            
-            
-
-            
-              
+        if(achou == false){
+            i++;
+            v = new int[v.length*10];
         }
             
 
-                tempoFinal = System.currentTimeMillis() - tempoInicial;
-                tabela[i] = tempoFinal;
-                System.out.println(tempoFinal);
+            tempoFinal = System.currentTimeMillis() - tempoInicial;
+            tabela[i] = tempoFinal;
                 
+
+         return buscaBinaria(v, tabela,ler,i);
+    }
+
+    public static long[] buscaExaustiva(int v[],long tabela[],Scanner ler,int i){
+
+        int chave;
+        chave = ler.nextInt();
+        long tempoInicial = 0;
+        long tempoFinal = 0;
+        boolean achou = false;
+        Random aleatorio = new Random();
 
         if(v.length == 1000000){
             return tabela;
         }
 
-             
+        
 
-         return buscaBinaria(v, tabela,ler,i);
+        for(int j = 0;j < v.length;j++){
+            v[j] = aleatorio.nextInt(1000);
+        }
+
+
+        System.out.print("Digite o numero que esta procurando: ");
+        chave = ler.nextInt();     
+
+        tempoInicial = System.currentTimeMillis();
+
+        for(int j = 0;j < v.length;j++){
+            if(chave == v[j]){
+                v = new int[v.length*10];
+                i++;
+                achou = true;
+                break;
+            }
+
+        }
+
+        if(achou == false){
+            v = new int[v.length*10];
+            i++;
+        }
+
+        tempoFinal = System.currentTimeMillis() - tempoInicial;
+        tabela[i] = tempoFinal;
+        return buscaBinaria(v, tabela,ler,i);
     }
+
+
 }
